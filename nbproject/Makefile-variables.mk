76dd50ea3dc3fce90e81f4042fe1d78419e6a0f7
Ferenc Pesti <fpesti@cofe.ddns.net>
2021-12-28 15:02:39 +0100

    Adding empty MPXLab project for PIC16LF15323

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=SIPF_RoastR.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/SIPF_RoastR.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=sipfroastr.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/sipfroastr.tar
